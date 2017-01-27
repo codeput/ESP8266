b9631f8644e4af55fba1ac3a0c8597b9755dd477
codeput <emaxduck@gmail.com>
2017-01-27 11:07:38 +0100

    Update Thermal_Control.ino

--- Thermal_Control/Thermal_Control.ino
+++ Thermal_Control/Thermal_Control.ino
@@ -1,57 +1,54 @@
 
 
 /* Controllo caldaia Principi Emanuele 2017 
-con ESP8266
 */
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <SimpleTimer.h>
-#include <dht11.h>
+//#include <dht11.h>
+#include "DHT.h"
 //Definizione variabili
-#define DHTPIN 4 //definizione del pin input sensore
-dht11 DHT11;
-#define RELEPIN 5 //definizione del pin controllo relay
+#define DHTPIN 1 //definizione del pin input sensore
+#define DHTTYPE DHT11   // DHT 11 
+//dht11 DHT11;
+DHT dht(DHTPIN, DHTTYPE,15);
+#define RELEPIN 2 //definizione del pin controllo relay
 int tempset ;
 int pinData;
-float h = 0;
-float t = 0;
+int h = 0;
+int t = 0;
 char auth[] = "0b6e3b4d57914fda8134cf980ef9b809";
 SimpleTimer timer;
 bool isFirstConnect = true;
 
-//Attivazione controllo
-BLYNK_WRITE(V6) 
-{
-  int pinData = param.asInt();
-}
-
 //Regolazione temperatura
+
 BLYNK_WRITE(V3) 
 {
   int tempset = param.asInt();
 }
 
 //Lettura e invio temperatura ambiente
+
 void sendUptime()
 {
-  DHT11.read(DHTPIN);
-  t = DHT11.temperature - 2 ;
+ // DHT11.read(DHTPIN);
+  t = dht.readTemperature();
   Blynk.virtualWrite(10, t);
-  h = DHT11.humidity ;
+  h = dht.readHumidity();
   Blynk.virtualWrite(11, h);
 }
 
 //Termostato ambiente
+
 void tc()
 {
-  DHT11.read(DHTPIN);
-  int ttc = DHT11.temperature - 2 ;
-  if ((ttc >= tempset) & (pinData == 1)) {
+  if ((t >= tempset) & (pinData == 1)) {
     digitalWrite(RELEPIN, false);
     Blynk.virtualWrite (4 , 0);
   }
-  else if ((ttc <= tempset) & (pinData == 1)) {
+  else if ((t <= tempset) & (pinData == 1)) {
     digitalWrite(RELEPIN, true);
     Blynk.virtualWrite (4 , 1000);
   }
@@ -61,6 +58,7 @@ void setup() {
   //  Serial.begin(115200);
   pinMode(RELEPIN, OUTPUT);
   pinMode(DHTPIN, INPUT);
+  dht.begin();
   Blynk.begin(auth, "Telecom-71694417", "qqqqqqqq", "emax.tcp4.me");
   timer.setInterval(5000L, sendUptime);
   timer.setInterval(3000L, tc);
